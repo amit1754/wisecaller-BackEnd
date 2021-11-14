80ecc85b4dd38c9a574cc42590057c431fc9ba6e
tushar-savaliya <devcentillion04@gmail.com>
2021-11-14 19:39:26 +0530

    update callhistory

--- call-service/src/controller/v1/callHistorory.ts
+++ call-service/src/controller/v1/callHistorory.ts
@@ -1,40 +1,33 @@
 import { Request, Response } from "express";
 import { map } from "lodash";
 import { UserContact } from "../../models/contactsync";
-import { User } from '../../models/user';
-import { CallHistory } from '../../models/callHistory';
-
+import { User } from "../../models/user";
+import { CallHistory } from "../../models/callHistory";
 
 class callHistory {
- 
   async add(req: Request, res: Response) {
-
     try {
-      const loginUser: any = req.user
-      let { body }: any = req
-   
+      const loginUser: any = req.user;
+      let { body }: any = req;
 
-      for(let i = 0; i < body.length; i++){
-        
-        let data=await User.findOne({mobileNo:body[i].callerId})
-        if(data)
-        {
-          body[i].number=body.callerId
-          body[i].callerId=data._id
-        }
-        else{
-          body[i].number=body[i].callerId
-          body[i].callerId=null
+      for (let i = 0; i < body.length; i++) {
+        let data = await User.findOne({ mobileNo: body[i].callerId });
+
+        if (data) {
+          body[i].number = body[i].callerId;
+          body[i].callerId = data._id;
+        } else {
+          body[i].number = body[i].callerId;
+          body[i].callerId = null;
         }
 
-        body[i].wisecallerId=loginUser._id
-        
-        const callHistorySave= new CallHistory(body[i])
-        await callHistorySave.save()
+        body[i].wisecallerId = loginUser._id;
+
+        const callHistorySave = new CallHistory(body[i]);
+        await callHistorySave.save();
       }
-      
-      res.status(200).json({ success: true, message: "Sucess", data: [] });
 
+      res.status(200).json({ success: true, message: "Sucess", data: [] });
     } catch (error: any) {
       res.status(500).json({ success: false, message: error.message });
     }
@@ -42,35 +35,96 @@ class callHistory {
 
   async callDetails(req: Request, res: Response) {
     try {
-      const loginUser: any = req.user
+      const loginUser: any = req.user;
 
-      const contactDetails = await CallHistory.find({wisecallerId:loginUser._id}).sort({ updatedAt: -1 })
-                      
-                        
+      const contactDetails = await CallHistory.aggregate([
+        { $match: { wisecallerId: loginUser._id } },
+        {
+          $lookup: {
+            from: "customstatuses",
+            localField: "callerId",
+            foreignField: "user",
+            as: "status",
+          },
+        },
+        {
+          $unwind: {
+            path: "$status",
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+        {
+          $lookup: {
+            from: "customstatuses",
+            localField: "callerId",
+            foreignField: "user",
+            as: "status",
+          },
+        },
+        {
+          $unwind: {
+            path: "$status",
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+        {
+          $lookup: {
+            from: "users",
+            localField: "callerId",
+            foreignField: "_id",
+            as: "userDetails",
+          },
+        },
 
-      
-      res.status(200).json({ success: true, message: "Sucess", data: contactDetails });
+        {
+          $group: {
+            _id: { $dateToString: { format: "%Y-%m-%d", date: "$date" } },
+            list: { $push: "$$ROOT" },
+          },
+        },
+        { $sort: { _id: -1 } },
+        {
+          $project: {
+            "list.wisecallerId": 1,
+            "list.callerId": 1,
+            "list.name": 1,
+            "list.callHistory": 1,
+            "list.date": 1,
+            "list.createdAt": 1,
+            "list.updatedAt": 1,
+            "list.number": 1,
+            "list.notes": "$list.status.notes",
+            "list.userDetails": 1,
+          },
+        },
+      ]);
+
+      res
+        .status(200)
+        .json({ success: true, message: "Sucess", data: contactDetails });
     } catch (error: any) {
       res.status(500).json({ success: false, message: error.message });
     }
   }
   async addFavorite(req: Request, res: Response) {
     try {
-      const loginUser: any = req.user
-      const { number }: any = req.body
-      const userFind = await UserContact.findOne({ user: loginUser._id })
+      const loginUser: any = req.user;
+      const { number }: any = req.body;
+      const userFind = await UserContact.findOne({ user: loginUser._id });
       const { contact } = userFind;
       const updateContact = map(contact, function (x) {
         if (x.number === number) {
-          x.isFavorite = true
-        }
-        else {
-          x.isFavorite = false
+          x.isFavorite = true;
+        } else {
+          x.isFavorite = false;
         }
         return x;
-      })
+      });
 
-      await UserContact.updateOne({ user: loginUser._id }, { contact: updateContact })
+      await UserContact.updateOne(
+        { user: loginUser._id },
+        { contact: updateContact }
+      );
       res.status(200).json({ success: true, message: "Sucess", data: [] });
     } catch (error: any) {
       res.status(500).json({ success: false, message: error.message });
@@ -78,118 +132,108 @@ class callHistory {
   }
   async addBlock(req: Request, res: Response) {
     try {
-      const loginUser: any = req.user
-      const { number }: any = req.body
-      const userFind = await UserContact.findOne({ user: loginUser._id })
+      const loginUser: any = req.user;
+      const { number }: any = req.body;
+      const userFind = await UserContact.findOne({ user: loginUser._id });
       const { contact } = userFind;
       const updateContact = map(contact, function (x) {
         if (x.number === number) {
-          x.isBlock = true
-        }
-        else {
-          x.isBlock = false
+          x.isBlock = true;
+        } else {
+          x.isBlock = false;
         }
         return x;
-      })
+      });
 
-      await UserContact.updateOne({ user: loginUser._id }, { contact: updateContact })
+      await UserContact.updateOne(
+        { user: loginUser._id },
+        { contact: updateContact }
+      );
       res.status(200).json({ success: true, message: "Sucess", data: [] });
     } catch (error: any) {
       res.status(500).json({ success: false, message: error.message });
     }
   }
 
-  async addNumber(req: Request, res: Response){
+  async addNumber(req: Request, res: Response) {
     try {
-      let reqestData:any =req.body
-      const loginUser: any = req.user
-      let callerHistory:any = await CallHistory.findOne({number:reqestData.callerId})
-      if(callerHistory){
-        let callerUpdate={
-          time:reqestData.time,
-          type:reqestData.type,
-          simId:reqestData.simId
-        }
-        callerHistory.callHistory.push(callerUpdate)
-         let user = await CallHistory.findOneAndUpdate(
-            { _id: callerHistory._id },
-            callerHistory,
-            {
-              upsert: true,
-              new: true,
-            }
-          );
-      return res.status(200).json({ success: true, data: user });
-        
-      }
-      else{
-        
-        reqestData.callHistory={
-          time:reqestData.time,
-          type:reqestData.type,
-          simId:reqestData.simId
-        }
-        
+      let reqestData: any = req.body;
+      const loginUser: any = req.user;
+      let callerHistory: any = await CallHistory.findOne({
+        number: reqestData.callerId,
+      });
+      if (callerHistory) {
+        let callerUpdate = {
+          time: reqestData.time,
+          type: reqestData.type,
+          simId: reqestData.simId,
+        };
+        callerHistory.callHistory.push(callerUpdate);
+        let user = await CallHistory.findOneAndUpdate(
+          { _id: callerHistory._id },
+          callerHistory,
+          {
+            upsert: true,
+            new: true,
+          }
+        );
+        return res.status(200).json({ success: true, data: user });
+      } else {
+        reqestData.callHistory = {
+          time: reqestData.time,
+          type: reqestData.type,
+          simId: reqestData.simId,
+        };
 
-        let data=await User.findOne({mobileNo:reqestData.callerId})
-        if(data)
-        {
-          reqestData.number=reqestData.callerId
-          reqestData.callerId=data._id
-        }
-        else{
-          reqestData.number=reqestData.callerId
-          reqestData.callerId=null
+        let data = await User.findOne({ mobileNo: reqestData.callerId });
+        if (data) {
+          reqestData.number = reqestData.callerId;
+          reqestData.callerId = data._id;
+        } else {
+          reqestData.number = reqestData.callerId;
+          reqestData.callerId = null;
         }
 
-        reqestData.wisecallerId=loginUser._id
-        
-        const callHistorySave= new CallHistory(reqestData)
-        await callHistorySave.save()
+        reqestData.wisecallerId = loginUser._id;
+
+        const callHistorySave = new CallHistory(reqestData);
+        await callHistorySave.save();
       }
 
-
-      
-
-   
-      
-      res.status(200).json({ success: true, message: "success",callLogs:reqestData });
-      
-    } catch (error:any) {
+      res
+        .status(200)
+        .json({ success: true, message: "success", callLogs: reqestData });
+    } catch (error: any) {
       res.status(500).json({ success: false, message: error.message });
     }
   }
-  async deleteNumber(req: Request, res: Response){
+  async deleteNumber(req: Request, res: Response) {
     try {
-      const requestData =req.body
-      const loginUser: any = req.user
-      const {callDetails}=requestData
+      const requestData = req.body;
+      const loginUser: any = req.user;
+      const { callDetails } = requestData;
 
-      const contactDetailsGet = await CallHistory.findOne({callLogs: {$elemMatch: {_id:requestData.callLogId}}})
+      const contactDetailsGet = await CallHistory.findOne({
+        callLogs: { $elemMatch: { _id: requestData.callLogId } },
+      });
 
-
-      const a=map(contactDetailsGet.callLogs, function(o:any) {
-      
-        if(o._id==requestData.callLogId)
-        { 
-          o.callList.pop()
+      const a = map(contactDetailsGet.callLogs, function (o: any) {
+        if (o._id == requestData.callLogId) {
+          o.callList.pop();
         }
         return o;
-      })
-    await   CallHistory.findOneAndUpdate(
-        { user: loginUser._id},
-        {callLogs:a},
-        {upsert:true,new:true})
-      
-      res.status(200).json({ success: true, message: "success",data:[]});
-      
-    } catch (error:any) {
+      });
+      await CallHistory.findOneAndUpdate(
+        { user: loginUser._id },
+        { callLogs: a },
+        { upsert: true, new: true }
+      );
+
+      res.status(200).json({ success: true, message: "success", data: [] });
+    } catch (error: any) {
       res.status(500).json({ success: false, message: error.message });
     }
   }
-
-
 }
-
 
 export default callHistory;
--- call-service/src/models/callHistory.ts
+++ call-service/src/models/callHistory.ts
@@ -1,38 +1,37 @@
 import { Schema, model } from "mongoose";
 
 const CallHistorySchema = new Schema(
-    {
-        wisecallerId:{
-            type: Schema.Types.ObjectId,
-            ref: "User",
-            default:null
-        },
-        callerId:{
-            type: Schema.Types.ObjectId,
-            ref: "User",
-            default:null
-        },
-        name:{
-            type: Schema.Types.String,
-        },
-        number:{
-            type: Schema.Types.String,
-        },
-     
-        callHistory:[
-            {
-                time:{type:String},
-                type:{type:String},
-                simId:{type:String},
-            }
-        ],
-      
-        date:{
-            type:Schema.Types.Number,
-        }
-
+  {
+    wisecallerId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
     },
-    { timestamps: true }
+    callerId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+    name: {
+      type: Schema.Types.String,
+    },
+    number: {
+      type: Schema.Types.String,
+    },
+
+    callHistory: [
+      {
+        time: { type: String },
+        type: { type: String },
+        simId: { type: String, default: "sim1" },
+      },
+    ],
+
+    date: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
 );
 
 export const CallHistory = model("user_call_history", CallHistorySchema);
