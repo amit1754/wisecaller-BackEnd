859a52b879dd9258aff0c42eae20cb1ea7f84ab8
Jignesh Foon <devcentillion04@gmail.com>
2022-02-15 14:56:42 +0530

    update package

--- Business-Components/user-service-pkg/src/bll/authToken.bll.ts
+++ Business-Components/user-service-pkg/src/bll/authToken.bll.ts
@@ -1,13 +1,12 @@
 import * as authToken from "../dal/authToken.dal";
 export default class AuthTokenBLL {
-
-async getTokenByPhone(phone: any):Promise<any>{
-  return await authToken.getUserToken(phone);
-}
-async findOneAndUpdate(phone: any, payload:any,options:any):Promise<any>{
-  return await authToken.findOneAndUpdate(phone, payload,options);
-}
-async createToken(payload: any):Promise<any>{
-  return await authToken.createToken(payload);
-}    
+  async getTokenByPhone(phone: any): Promise<any> {
+    return await authToken.getUserToken(phone);
+  }
+  async findOneAndUpdate(phone: any, payload: any, options: any): Promise<any> {
+    return await authToken.findOneAndUpdate(phone, payload, options);
+  }
+  async createToken(payload: any): Promise<any> {
+    return await authToken.createToken(payload);
+  }
 }
--- Business-Components/user-service-pkg/src/dal/authToken.dal.ts
+++ Business-Components/user-service-pkg/src/dal/authToken.dal.ts
@@ -1,29 +1,25 @@
-
-
 import { AuthToken } from "../models/auth-token";
 
-export const getUserToken = async (username: any):Promise<any>=> {
+export const getUserToken = async (username: any): Promise<any> => {
   return await AuthToken.findOne({
-    $or: [
-      { mobileNo: username },
-      { email: username}
-    ]
+    $or: [{ mobileNo: username }, { email: username }],
   });
 };
 
-export const findOneAndUpdate = async (username: any, payload:any,options:any):Promise<any>=> {
+export const findOneAndUpdate = async (
+  username: any,
+  payload: any,
+  options: any
+): Promise<any> => {
   return await AuthToken.findOneAndUpdate(
-    { $or: [
-      { mobileNo: username },
-      { email: username}
-    ] },
+    { $or: [{ mobileNo: username }, { email: username }] },
     payload,
     options
   );
 };
 
-export const createToken = async (payload:any):Promise<any>=>{
-    let token = new AuthToken(payload);
+export const createToken = async (payload: any): Promise<any> => {
+  let token = new AuthToken(payload);
   await token.save();
   return token;
 };
