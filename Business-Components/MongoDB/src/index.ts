9a3db83d38cfbd2d989fbaaced5c5868df3c0e70
Jignesh Foon <devcentillion04@gmail.com>
2022-04-19 12:42:45 +0530

    upadate mongodb package

--- Business-Components/MongoDB/src/index.ts
+++ Business-Components/MongoDB/src/index.ts
@@ -1,10 +1,10 @@
 require('dotenv').config();
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 mongoose
   .connect(`${process.env.MONGOURL}${process.env.MONGODB}?authSource=admin`)
   .then(() => {
-    console.log("MONGODB CONNECTED");
+    // console.log("MONGODB CONNECTED");
   })
   .catch((error) => {
     console.log(error.message);
