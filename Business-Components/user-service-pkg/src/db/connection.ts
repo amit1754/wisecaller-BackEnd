5dc0643b9aeadc580c4b849c121c84c0aa060c89
nithinabrahampaul <nithinabrahampaul@gmail.com>
2022-04-19 18:28:27 +0530

    added missing files

---
+++ Business-Components/user-service-pkg/src/db/connection.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+require('dotenv').config();
+
+mongoose
+  .connect(`${process.env.MONGOURL}${process.env.MONGODB}?authSource=admin`)
+  .then(() => {
+    console.log("MONGODB CONNECTED");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
